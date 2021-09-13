a50e241ec09e4d5e2dd42eec93882f34a07aa109
Jaren Provost <jxprovost@bsu.edu>
2021-09-13 19:03:50 -0400

    Make more progress to button layout

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
+
 public class UserInterface {
 
     public static void main(String[] args) {
@@ -11,7 +12,11 @@ public class UserInterface {
 
         ui.setLayout(new BorderLayout());
 
-        //Visible Display
+        //Visible Display...
+
+        JPanel window = new JPanel();
+        window.setPreferredSize(new Dimension(600,100));
+        //JOptionPane window = new JOptionPane();
 
 
         //4 Button Pannel
@@ -19,33 +24,95 @@ public class UserInterface {
         JPanel pannel1 = new JPanel();
         JPanel pannel2 = new JPanel();
         JPanel pannel3 = new JPanel();
-
+        Dimension numberButtons = new Dimension(200, 200);
         JButton button0 = new JButton(" 0 ");
+        button0.setPreferredSize(numberButtons);
         JButton button1 = new JButton(" 1 ");
+        button1.setPreferredSize(numberButtons);
         JButton button2 = new JButton(" 2 ");
+        button2.setPreferredSize(numberButtons);
         JButton button3 = new JButton(" 3 ");
+        button3.setPreferredSize(numberButtons);
 
         pannel0.add(button0);
         pannel1.add(button1);
         pannel2.add(button2);
         pannel3.add(button3);
 
-        JPanel buttonsPannel = new JPanel();
-        buttonsPannel.setLayout(new GridLayout(2,2));
+        JPanel numberButtonsPannel = new JPanel();
+        numberButtonsPannel.setLayout(new GridLayout(2,2));
 
-        buttonsPannel.add(pannel0);
-        buttonsPannel.add(pannel1);
-        buttonsPannel.add(pannel2);
-        buttonsPannel.add(pannel3);
+        numberButtonsPannel.add(pannel0);
+        numberButtonsPannel.add(pannel1);
+        numberButtonsPannel.add(pannel2);
+        numberButtonsPannel.add(pannel3);
+
+
 
         //Operations "+","-","*","/" (EAST)
+        JPanel pannel_add = new JPanel();
+        JPanel pannel_sub = new JPanel();
+        JPanel pannel_div = new JPanel();
+        JPanel pannel_mul = new JPanel();
+
+        Dimension operationButtons = new Dimension(140, 60);
+        JButton button_add = new JButton(" + ");
+        button_add.setPreferredSize(operationButtons);
+        JButton button_sub = new JButton(" - ");
+        button_sub.setPreferredSize(operationButtons);
+        JButton button_div = new JButton(" / ");
+        button_div.setPreferredSize(operationButtons);
+        JButton button_mul = new JButton(" * ");
+        button_mul.setPreferredSize(operationButtons);
+
+        pannel_add.add(button_add, BorderLayout.EAST);
+        pannel_sub.add(button_sub, BorderLayout.EAST);
+        pannel_div.add(button_div, BorderLayout.EAST);
+        pannel_mul.add(button_mul, BorderLayout.EAST);
+
+        JPanel operationsPannel = new JPanel();
+        operationsPannel.setLayout(new GridLayout(4,1));
+
+        operationsPannel.add(pannel_add, BorderLayout.EAST);
+        operationsPannel.add(pannel_sub, BorderLayout.EAST);
+        operationsPannel.add(pannel_div, BorderLayout.EAST);
+        operationsPannel.add(pannel_mul, BorderLayout.EAST);
+
+
 
         // "=" (SOUTH)
+        Dimension bottomButtons = new Dimension(260, 60);
+
+        JPanel pannel_equals = new JPanel();
+        JButton button_equals = new JButton(" = ");
+        button_equals.setPreferredSize(bottomButtons);
+        pannel_equals.add(button_equals);
+
+        JPanel equal_pannel = new JPanel();
+        equal_pannel.setLayout(new GridLayout(1,1));
+        equal_pannel.add(pannel_equals);
+
 
         //Toggle (SOUTH)
+        JPanel pannel_toggle = new JPanel();
+        JButton button_toggle = new JButton(" toggle ");
+        button_toggle.setPreferredSize(bottomButtons);
+        pannel_toggle.add(button_toggle);
 
-        ui.add(buttonsPannel, BorderLayout.CENTER);
+        JPanel toggle_pannel = new JPanel();
+        toggle_pannel.setLayout(new GridLayout(1,1));
+        toggle_pannel.add(pannel_toggle);
 
+        JPanel bottomPannel = new JPanel();
+        bottomPannel.add(equal_pannel);
+        bottomPannel.add(toggle_pannel);
+
+        ui.add(window,BorderLayout.NORTH);
+        ui.add(numberButtonsPannel, BorderLayout.CENTER);
+        ui.add(operationsPannel, BorderLayout.EAST);
+        ui.add(bottomPannel, BorderLayout.SOUTH);
+        ui.getLayeredPane();
+        ui.pack();
         ui.setVisible(true);
     }
 
